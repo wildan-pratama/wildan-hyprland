337c6b6cd5571fd629ec3ded1e9b93419acc07e0
Wildan Pratama <willdanpratamaa@gmail.com>
2023-02-11 16:11:12 +0700

    Initial commit

---
+++ .config/autostart/autostart.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+## Copyright (C) 2020-2023 Aditya Shakya <adi1090x@gmail.com>
+##
+## Autostart Programs
+
+# Kill already running process
+_ps=(swaybg swayidle waybar dunst mpd xdg-desktop-portal-hyprland xdg-desktop-portal xfce-polkit)
+for _prs in "${_ps[@]}"; do
+	if [[ `pidof ${_prs}` ]]; then
+		killall -9 ${_prs}
+	fi
+done
+
+# Polkit agent
+/usr/lib/xfce-polkit/xfce-polkit &
+
+# Start mpd
+exec mpd &
+
+# Swayidle
+exec swayidle -w timeout 1800 '~/.config/hypr/bin/lock.sh' timeout 60 'systemctl suspend' &
+
+# Etc
+sleep 1
+/usr/libexec/xdg-desktop-portal-hyprland &
+sleep 2
+/usr/lib/xdg-desktop-portal &
+xdg-user-dirs-update
---
+++ README.md
@@ -0,0 +1,2 @@
+# wildan-hyprland
+ 
