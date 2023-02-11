0e2a0d873f63bcde648903f8b9a4a3accce0ff61
Wildan Pratama <kadalgurun521@gmail.com>
2023-02-11 23:23:19 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
-yay -S git sddm-git polkit hyprland-bin waybar-hyprland alacritty rofi nemo gvfs-mtp file-roller swaybg swaylock-effects-git wl-clipboard mailcap mpv \
-networkmanager-dmenu-git xfce-polkit dunst geany viewnior nwg-look xdg-desktop-portal-hyprland-git qt5-svg inetutils mpc mpd ncmpcpp xdg-user-dirs \
-pulsemixer pavucontrol qt5-graphicaleffects qt5-quickcontrols2 pipewire wireplumber grim slurp jq dunst qt5-wayland qt6-wayland
+yay -S git sddm-git polkit hyprland-bin waybar-hyprland alacritty rofi nemo gvfs-mtp swayidle \
+file-roller swaybg swaylock-effects-git wl-clipboard mailcap mpv networkmanager-dmenu-git \
+xfce-polkit dunst geany viewnior nwg-look xdg-desktop-portal-hyprland-git qt5-svg inetutils \
+mpc mpd ncmpcpp xdg-user-dirs pulsemixer pavucontrol qt5-graphicaleffects qt5-quickcontrols2 \
+pipewire wireplumber grim slurp jq dunst qt5-wayland qt6-wayland
 yay -Rs foot xdg-desktop-portal-gnome
 
 sudo cp -a usr /
