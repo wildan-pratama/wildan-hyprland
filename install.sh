ddb77cd01e21725a06bccc68bbfce2d074e940cd
Wildan Pratama <kadalgurun521@gmail.com>
2023-02-12 12:12:54 +0700

    Add keyboards theme

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ yay -S git sddm-git polkit hyprland-bin waybar-hyprland alacritty rofi nemo gvfs
 file-roller swaybg swaylock-effects-git wl-clipboard mailcap mpv networkmanager-dmenu-git \
 xfce-polkit dunst geany viewnior nwg-look xdg-desktop-portal-hyprland-git qt5-svg inetutils \
 mpc mpd ncmpcpp xdg-user-dirs pulsemixer pavucontrol qt5-graphicaleffects qt5-quickcontrols2 \
-pipewire wireplumber grim slurp jq dunst qt5-wayland qt6-wayland
+pipewire wireplumber grim slurp jq dunst qt5-wayland qt6-wayland pastel
 yay -Rs foot xdg-desktop-portal-gnome
 
 sudo cp -a usr /
