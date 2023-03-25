7d81b53d5547f4b28c565e5928e5b6f07afde8f8
Wildan Pratama <kadalgurun521@gmail.com>
2023-03-25 15:34:49 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -14,7 +14,8 @@ hypr_pkgs () {
     gtk-engine-murrine dunst nwg-look xdg-desktop-portal-hyprland-git qt5-svg mpc mpd ncmpcpp inetutils\
     pamixer bluez-utils xdg-user-dirs pulsemixer pavucontrol qt5-graphicaleffects networkmanager jq dunst \
     qt5-quickcontrols2 brightnessctl noto-fonts network-manager-applet pipewire wireplumber grim slurp \
-    qt5-wayland qt6-wayland pastel  noto-fonts-emoji wdisplays swww xorg-xhost gvfs gvfs-mtp ntfs-3g ttf-jetbrains-mono-nerd
+    qt5-wayland qt6-wayland pastel  noto-fonts-emoji wdisplays swww xorg-xhost gvfs gvfs-mtp ntfs-3g \
+    ttf-jetbrains-mono-nerd noto-fonts-cjk
 }
 
 gui_pkgs () {
