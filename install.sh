5a19eea24d5c38cf2ace0396669cc603de542376
wildan-pratama <willdanpratamaa@gmail.com>
2023-07-05 09:33:43 +0700

    update

--- install.sh
+++ install.sh
@@ -9,22 +9,26 @@ cek_update () {
 hypr_pkgs () {
     echo "Installling hyprland pkgs"
     echo
-    yay -Syy polkit hyprland-git waybar-hyprland-git alacritty rofi-lbonn-wayland-git swayidle bc \
-    swaybg swaylock-effects-git wl-clipboard mailcap networkmanager-dmenu-git wf-recorder xfce-polkit\
-    gtk-engine-murrine dunst nwg-look xdg-desktop-portal-hyprland-git qt5-svg mpc mpd ncmpcpp inetutils\
-    pamixer bluez-utils xdg-user-dirs pulsemixer pavucontrol qt5-graphicaleffects networkmanager jq dunst \
-    qt5-quickcontrols2 brightnessctl noto-fonts network-manager-applet pipewire wireplumber grim slurp \
-    qt5-wayland qt6-wayland pastel  noto-fonts-emoji wdisplays swww xorg-xhost gvfs gvfs-mtp ntfs-3g \
-    ttf-jetbrains-mono-nerd noto-fonts-cjk kvantum
+
+    # Hyprland Pkgs
+    yay -Syy polkit hyprland-git waybar-hyprland-git alacritty rofi-lbonn-wayland-git swayidle hyprshot \
+    swaybg swaylock-effects-git wl-clipboard wl-clipboard-history-git xdg-desktop-portal-hyprland-git
+
+    # Network manager
+    yay -S networkmanager-dmenu-git network-manager-applet networkmanager
+
+    # Media & backend
+    yay -S mailcap wf-recorder xfce-polkit bc gtk-engine-murrine dunst nwg-look qt5-svg \
+    inetutils pamixer bluez-utils xdg-user-dirs qt5-graphicaleffects xorg-xhost xsettingsd \
+    gvfs gvfs-mtp ntfs-3g jq dunst qt5-quickcontrols2 brightnessctl kvantum qt5-wayland qt6-wayland pastel \
+    pulsemixer pavucontrol mpc mpd ncmpcpp pipewire wireplumber grim slurp wdisplays \
+    noto-fonts noto-fonts-emoji noto-fonts-cjk ly
 }
 
 gui_pkgs () {
-    yay -Syy thunar thunar-archive-plugin thunar-volman file-roller mpv geany viewnior 
+    yay -Syy thunar thunar-archive-plugin thunar-volman file-roller mpv geany loupe 
 }
 
-rm_pkgs () {
-    yay -Rs xdg-desktop-portal-gnome
-}
 
 cp_theme () {
     sudo cp -r usr /
@@ -48,12 +52,6 @@ backup_conf () {
     mv "$DIR"/hypr "$DIR"/hypr.bak
     cp_conf
     fi
-}
-
-sddm_pkgs () {
-    yay -Syy sddm
-    sudo rm -rf /etc/systemd/system/display-manager.service
-    sudo systemctl enable sddm
 }
 
 install () {
@@ -83,7 +81,6 @@ install () {
     gui_pkgs
     backup_conf
     cp_theme
-    rm_pkgs
     ;;
   *)
     echo "Your choice not valid"
